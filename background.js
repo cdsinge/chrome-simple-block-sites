f06d2d05b3e352e3deb76eeffecc9d2e405ffc50
cdsinge <1156547+cdsinge@users.noreply.github.com>
2019-07-14 13:11:34 +0100

    Avoid some cases that break Chrome, e.g. matching empty-string./nMinor styling of options.

--- background.js
+++ background.js
@@ -1,16 +1,35 @@
 'use strict';
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-   var unwantedBaseUrls = chrome.storage.sync.get({sitesToBlockCSV: ''}).split(/,/);
-   var chosenURL = changeInfo.url;
-   if (chosenURL === undefined) {
-	   return;
-   }
+    chrome.storage.sync.get({'sitesToBlockCSV': ''},
+        function(result) {
+            var unwantedBaseUrls = result.sitesToBlockCSV.split(/,/);
 
-   var isPresent = unwantedBaseUrls.some(function(banned) { return chosenURL.indexOf(banned) >= 0; })
-   if (isPresent) {
-	  alert("You wanted: " + chosenURL + "\nDisallowed by extension.");
-	  var newURL = 'http://www.google.com';
-      chrome.tabs.update(tabId, {url:newURL});
-   }
+            // Avoid really short substrings, since we're not matching sensibly
+            for (let i=unwantedBaseUrls.length-1; i>=0; i--) {
+                if (unwantedBaseUrls[i].length < 5) {
+                    unwantedBaseUrls.splice(i, 1);
+                }
+            }
+
+            var chosenURL = changeInfo.url;
+            if (chosenURL === undefined) {
+                return;
+            }
+            if (chosenURL.includes('chrome')) {
+                // in case CSV is very badly specified
+                // try to keep urls for disabling extension
+                return;
+            }
+
+            var isPresent = unwantedBaseUrls.some(function(banned) {
+                return chosenURL.indexOf(banned) >= 0; }
+            )
+            if (isPresent) {
+                alert("You wanted: " + chosenURL + "\nDisallowed by extension.");
+                var newURL = 'http://www.google.com';
+                chrome.tabs.update(tabId, {url:newURL});
+            }
+        }
+    );
 });
